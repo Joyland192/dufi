0706a885daea463ece5ba5db026f6d5bc5b38107
JoyLand192 <cyw4642@gmail.com>
2020-08-15 18:37:56 +0900

    d

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require(`discord.js`); // discord.js를 불러옴
+const client = new Discord.Client(); // 새로운 디스코드 클라이언트를 만듬
+const token = process.env.token;
+
+client.once('ready', () => {
+	console.log("Bot Login.");
+	client.user.setActivity(`두피쿤                                `);
+});
+
+client.login(token);
