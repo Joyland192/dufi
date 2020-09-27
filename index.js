70e3509058640cbcc81e434f33b2f4f84275c54d
JoyLand192 <cyw4642@gmail.com>
2020-09-27 17:19:04 +0900

    d

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require(`discord.js`); // discord.js를 불러옴
 const client = new Discord.Client(); // 새로운 디스코드 클라이언트를 만듬
-//const token = process.env.token;
+const token = process.env.token;
 
 client.once('ready', () => {
 	console.log("Bot Login.");
@@ -8,11 +8,11 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
-	if (message.channel.id == `699698079355502634`) {
+	if (message.channel.id == `759689892174233611`) {
     	if (message.content === `다바피보`) {
     	    message.channel.send(`바부`);
 		}
 	}
 });
 
-client.login("NzQ0MTMzMTg2NDAwODEzMTM2.XzeyCA.3CkYPkUyeP4gim_BeDRxztbxWBI");
+client.login(token);
